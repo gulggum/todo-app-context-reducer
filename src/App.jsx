7ed3ae0003179afb37909d707741af410afdb23c
gulggum <163222604+gulggum@users.noreply.github.com>
2025-10-20 19:14:26 +0900

    기본 todo UI제작

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>Todo List</h1>
+      <div>
+        <h1>Todo List</h1>
+
+        <form>
+          <input type="text" placeholder="Please write todo..." />
+          <button type="submit">입력</button>
+        </form>
+        <ul>
+          <li>
+            <span>리액트 공부하기</span>
+            <button>수정</button>
+            <button>삭제</button>
+          </li>
+          <li>
+            <span>운동하기</span>
+            <button>수정</button>
+            <button>삭제</button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
