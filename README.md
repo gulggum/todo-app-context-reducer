5bb2e2fc658ac0dc98952ff7e9a4fe2a523e184e
gulggum <163222604+gulggum@users.noreply.github.com>
2025-10-20 14:32:04 +0900

    init:첫커밋

---
+++ README.md
@@ -0,0 +1,4 @@
+# Context API와 useReducer를 활용한 React Todo List 프로젝트
+
+- 25/10/20 start!
+- 개발중...
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Todo List</h1>
+    </>
+  );
+}
+
+export default App;
